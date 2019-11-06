b89e19b3a2ea64d53a5dc67ff3abc20050bc3091
Dany Benavides Pedroza <danybenavidespedroza@Danys-MacBook-Pro.local>
2019-11-06 07:26:56 -0500

    Variables modelo

--- Entrega2Proyecto.py
+++ Entrega2Proyecto.py
@@ -24,5 +24,22 @@ sexos = Range(1,numSexos)
 idiomas = Range(1,numIdiomas)
 sexos = Range(1,numSexos)
 edad = Range(1,numEdades)
-nicho = Range(1,numNicho)
+nichos = Range(1,numNicho)
 ubicaciones = Range(1,numUbicaciones)
+
+
+
+
+
+
+
+#variables
+
+Model.objetivo = Var(objetivos, domain=PositiveIntegers)
+Model.sexo = Var(sexos, domain=PositiveIntegers)
+Model.idioma = Var(idiomas, domain=PositiveIntegers)
+Model.nicho = Var(nichos, domain=PositiveIntegers)
+Model.ubicacion = Var(ubicaciones, domain=PositiveIntegers)
+
+Model.x = Var(paises, domain=Binary)
+Model.y = Var(sexos, domain=Binary)
