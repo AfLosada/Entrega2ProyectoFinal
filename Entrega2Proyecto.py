bfc2be2a43fafa6f11add8735c6b7ee117f8bebf
Andrés Losada <nedrocoli@gmail.com>
2019-11-06 10:59:26 -0500

    Lo último que hicimos en clase

--- Entrega2Proyecto.py
+++ Entrega2Proyecto.py
@@ -26,31 +26,33 @@ numNichos = 3
 numUbicaciones = 2
 
 # Sets: cada entero representa un pais
-paises = RangeSet(1,numPaises)
-objetivos = RangeSet(1,numObjetivos)
-sexos = RangeSet(1,numSexos)
-idiomas = RangeSet(1,numIdiomas)
-edades = RangeSet(1,numEdades)
-nichos = RangeSet(1,numNichos)
-ubicaciones = RangeSet(1,numUbicaciones)
+Model.paises = RangeSet(1,numPaises)
+Model.objetivos = RangeSet(1,numObjetivos)
+Model.sexos = RangeSet(1,numSexos)
+Model.idiomas = RangeSet(1,numIdiomas)
+Model.edades = RangeSet(1,numEdades)
+Model.nichos = RangeSet(1,numNichos)
+Model.ubicaciones = RangeSet(1,numUbicaciones)
 
 #Parametros
 
-numeroHabitantes = Param(paises, mutable = True)
-costoObjetivo = Param(objetivos, mutable = True)
-porcentajeSexo = Param(sexos, mutable = True)
-porcentajeHablantes = Param(idiomas, mutable = True)
-porcentajeDentroNicho = Param(nichos, mutable = True)
-costoUbicacion = Param(ubicaciones, mutable = True)
-costoPorImpresion = Param(paises, mutable = True)
+Model.numeroHabitantes = Param(Model.paises, mutable = True)
+Model.costoObjetivo = Param(Model.objetivos, mutable = True)
+Model.porcentajeSexo = Param(Model.sexos, mutable = True)
+Model.porcentajeHablantes = Param(Model.idiomas, mutable = True)
+Model.porcentajeDentroNicho = Param(Model.nichos, mutable = True)
+Model.costoUbicacion = Param(Model.ubicaciones, mutable = True)
+Model.costoPorImpresion = Param(Model.paises, mutable = True)
 
 #variables
 
-Model.objetivo = Var(objetivos, domain=PositiveIntegers)
-Model.sexo = Var(sexos, domain=PositiveIntegers)
-Model.idioma = Var(idiomas, domain=PositiveIntegers)
-Model.nicho = Var(nichos, domain=PositiveIntegers)
-Model.ubicacion = Var(ubicaciones, domain=PositiveIntegers)
+Model.objetivo = Var(Model.objetivos, domain=PositiveIntegers)
+Model.sexo = Var(Model.sexos, domain=PositiveIntegers)
+Model.idioma = Var(Model.idiomas, domain=PositiveIntegers)
+Model.nicho = Var(Model.nichos, domain=PositiveIntegers)
+Model.ubicacion = Var(Model.ubicaciones, domain=PositiveIntegers)
 
-Model.x = Var(paises, domain=Binary)
-Model.y = Var(sexos, domain=Binary)
+Model.x = Var(Model.paises, domain=Binary)
+Model.y = Var(Model.sexos, domain=Binary)
+
+pob = sqrt(-Model.numeroHabitantes[p]*Model.x[p] for p  )
