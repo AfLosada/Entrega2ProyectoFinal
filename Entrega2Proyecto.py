b10eb2c3ac5282944b08bfc08b40245c8c8755bc
ANDRES FELIPE LOSADA LUNA <af.losada@sis.virtual.uniandes.edu.co>
2019-11-07 15:03:51 -0500

    Falta arreglar el solver

--- Entrega2Proyecto.py
+++ Entrega2Proyecto.py
@@ -12,6 +12,7 @@ from pyomo.opt import SolverFactory
 
 import sys
 import os
+import math
 
 os.system("clear")
 
@@ -22,7 +23,7 @@ numObjetivos = 5
 numSexos = 3
 numIdiomas = 2
 numEdades = 6
-numNichos = 3
+numNichos = 2
 numUbicaciones = 2
 presupuesto = 100000 # El presupuesto del ususario
 
@@ -31,7 +32,6 @@ Model.paises = RangeSet(1,numPaises)
 Model.objetivos = RangeSet(1,numObjetivos)
 Model.sexos = RangeSet(1,numSexos)
 Model.idiomas = RangeSet(1,numIdiomas)
-Model.edades = RangeSet(1,numEdades)
 Model.nichos = RangeSet(1,numNichos)
 Model.ubicaciones = RangeSet(1,numUbicaciones)
 
@@ -44,6 +44,39 @@ Model.porcentajeHablantes = Param(Model.idiomas, mutable = True)
 Model.porcentajeDentroDeNicho = Param(Model.nichos, mutable = True)
 Model.costoUbicacion = Param(Model.ubicaciones, mutable = True)
 Model.costoPorImpresion = Param(Model.paises, mutable = True)
+
+#Habitantes por Pais con Facebook
+Model.numeroHabitantes[1] = 33000000  #Colombia
+Model.numeroHabitantes[2] = 230000000 #E.E.U.U.
+Model.numeroHabitantes[3] = 44000000  #UK
+
+#Costo Objetivos
+Model.costoObjetivo[1] = 1   #Trafico
+Model.costoObjetivo[2] = 0.3 #Brand Awareness
+Model.costoObjetivo[3] = 0.8 #Traffic
+Model.costoObjetivo[4] = 1.4 #Lead Generation
+Model.costoObjetivo[5] = 2   #Conversions
+
+#Porcentaje de Cada Sexo con Facebook
+Model.porcentajeSexo[1] = 0.62
+Model.porcentajeSexo[2] = 0.38
+
+#Porcentaje de Hablantes 
+Model.porcentajeHablantes[1] = 0.52 #Ingles
+Model.porcentajeHablantes[2] = 0.16 #Español
+
+#Porcentaje Dentro De Nicho
+Model.porcentajeDentroDeNicho[1] = 0.03 #Viajero
+Model.porcentajeDentroDeNicho[2] = 0.013 #High Tech
+
+#Costo por Ubicacion
+Model.costoUbicacion[1] = 20/1000 #Facebook
+Model.costoUbicacion[2] = 28/1000 #Instagram
+
+#Costo por Impresion en dolares
+Model.costoPorImpresion[1] = 1/1000 #Colombia
+Model.costoPorImpresion[2] = 33/1000 #E.E.U.U.
+Model.costoPorImpresion[3] = 11.55/1000 #UK
 
 #variables
 
@@ -65,15 +98,15 @@ def costos2(u, p):
     return Model.costoUbicacion[u] * Model.ubicacion[u] + Model.costoPorImpresion[p]*Model.x[p]
 
 def porc1(i, n):
-    return Model.porcentajeHablantes(i)*Model.idioma[i] + Model.porcentajeDentroDeNicho[n]+Model.nicho[n]
+    return Model.porcentajeHablantes[i]*Model.idioma[i] + Model.porcentajeDentroDeNicho[n]+Model.nicho[n]
 #Funcion Objetivo
 Model.objetivo = Objective(expr = sum( ((pob(p)*costos1(p,o,s)*costos2(u,p)*porc1(i,n))/presupuesto) for p in Model.paises for o in Model.objetivos for s in Model.sexos for u in Model.ubicaciones for i in Model.idiomas for n in Model.nichos ))
 
 #Constraints
 
 #Solver
-
 SolverFactory('glpk').solve(Model)
+
 
 Model.display()
 
